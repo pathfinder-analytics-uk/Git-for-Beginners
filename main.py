7f2e643b416905ebdabf7cf369593572ba4e5c37
Malvik Vaghadia <admin@pathfinder-analytics.net>
2026-04-05 11:50:08 +0100

    added subtract_nums function and updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from utils.calculator import add_nums
+from utils.calculator import add_nums, subtract_nums
 
 if __name__ == "__main__":
     print(add_nums(1,3))
+    print(subtract_nums(5,3))
--- utils/calculator.py
+++ utils/calculator.py
@@ -1,2 +1,5 @@
 def add_nums(a,b):
     return a+b
+
+def subtract_nums(a,b):
+    return a-b
