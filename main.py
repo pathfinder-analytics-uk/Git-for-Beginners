c3b2cf78adaac3ec39144cf7734f43b91d406883
Malvik Vaghadia <admin@pathfinder-analytics.net>
2026-04-05 11:46:51 +0100

    added divide_nums function and updated main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from utils.calculator import add_nums
+from utils.calculator import add_nums, divide_nums
 
 if __name__ == "__main__":
     print(add_nums(1,3))
+    print(divide_nums(5,3))
--- utils/calculator.py
+++ utils/calculator.py
@@ -1,2 +1,5 @@
 def add_nums(a,b):
     return a+b
+
+def divide_nums(a,b):
+    return a/b
