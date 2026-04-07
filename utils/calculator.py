47b036acd72c080757718779bc263fde621b352b
Malvik Vaghadia <admin@pathfinder-analytics.net>
2026-04-07 12:44:36 +0100

    formatted calculator.py

--- utils/calculator.py
+++ utils/calculator.py
@@ -1,14 +1,14 @@
 def add_nums(a,b):
-    return a+b
+    return a + b
 
 def divide_nums(a,b):
-    return a/b
+    return a / b
 
 def subtract_nums(a,b):
-    return a-b
+    return a - b
 
 def power(a,b):
-    return a**b
+    return a ** b
 
 def multiply_nums(a,b):
     return a * b
